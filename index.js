e69919a5b3a19e05cec54e66961e73f21ade0021
Bethany  Walker <bethanymarciawalker@outlook.com>
2017-12-01 17:30:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+function array() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+}
+
+function destructivelyAppendKitten(name) {
+  array().push(name);
+  return array;  
+  
+}
